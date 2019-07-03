9eade23f2535b200f812273dfd4e15d8770dd576
Kalpit Khamar <kkk0482@njit.edu>
2019-07-02 22:56:09 -0400

    About page added

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Kalpit's Laravel
+                    Welcome to the homepage
                 </div>
 
                 <div class="links">
